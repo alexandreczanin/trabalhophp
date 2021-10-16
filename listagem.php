48d2c13a63ef8f646f603b2942d4537d2be19484
Alexandre C. Zanin <88693396+alexandreczanin@users.noreply.github.com>
2021-10-15 21:34:22 -0300

    Add files via upload

---
+++ listagem.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="content-language" content="pt-br">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <div class="row row-cols-2 row-cols-md-1 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-body">
+            <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">Nome:</th>
+                <th scope="col">E-mail:</th>
+                <th scope="col">Celular:</th>
+                <th scope="col">Cidade:</th>
+                <th scope="col">Data de Nascimento:</th>
+                <th scope="col">Ações:</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+              include 'conecta.php';
+              $pesquisa = mysqli_query($conn,"SELECT * FROM pessoa");
+              $row = mysqli_num_rows($pesquisa);
+              if($row > 0)
+              {
+                while($registro = $pesquisa->fetch_array()){
+                  $id = $registro['id'];
+                  echo '<tr>';
+                  echo '<td>'.$registro['nome'].'</td>';
+                  echo '<td>'.$registro['email'].'</td>';
+                  echo '<td>'.$registro['celular'].'</td>';
+                  echo '<td>'.$registro['cidade'].'</td>';
+                  echo '<td>'.$registro['data_nascimento'].'</td>';
+                  echo '<td><a href="edita.php?id='.$id.'"><button type="button" class="btn btn-outline-dark" type"button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                  </svg>
+                   Editar
+                  </button></a>  <a href="exclui.php?id='.$id.'"><button type="button" class="btn btn-outline-dark" type"button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                  </svg>
+                   Excluir
+                  </button></a></td>';
+                  echo '</tr>';
+                }
+                echo '</tbody>';
+                echo '</table>';
+              }
+              else {
+                echo "Não há registros para listar.";
+                echo "</tbody>";
+                echo "</table>";
+              }
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </body>
+</html>
